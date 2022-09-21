89152e6bc46d43e04f7f4db3d5b9632c3fa696ed
wiledwardmunoz.desarrollo <wiledwardmunoz.desarrollo@gmail.com>
2022-09-20 20:37:41 -0500

    homework1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,14 +1,31 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  let binario = 0; 
+  
+    for (let i = 0; i < num.length; i++) { 
+       binario += +num[i] * 2 ** (num.length - 1 - i); 
+    }
+    return binario;
 }
+
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let salidaBinaria = '';
+  if (num != 0) {
+    let decimal = num;
+    while (decimal != 0) {
+      salidaBinaria = (decimal % 2) + salidaBinaria;
+      decimal = Math.floor(decimal/2);
+    }
+  } else { salidaBinaria = "0";}
+  return salidaBinaria;
 }
+
+
+
+
 
 
 module.exports = {
